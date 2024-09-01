b04e876d59f3e7def0c39ada11b20188851decb6
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-09-01 18:25:42 -0400

    Update main.py

--- HW-Assignment 1/main.py
+++ HW-Assignment 1/main.py
@@ -15,14 +15,14 @@ with open("master.csv", mode='r') as master_file:
     csv_reader = csv.reader(master_file)
     next(csv_reader)
     for row in csv_reader:
-        customer_data.append(customer(row[0], row[1],  row[2], row[3]))
+        # customer_data.append(customer(row[0], row[1],  row[2], row[3]))
         customer_master[row[0]] = {
             "name": row[1],
             "balance": row[2],
             "discount": row[3]
         }
 
-pprint(customer_master)
+# pprint(customer_master)
 
 # for cus in customer_data:
 #     print(cus)
@@ -45,5 +45,58 @@ with open("transactions.csv", mode='r') as transactions_file:
 # for transactions in transactions_history:
 #     print(transactions)
 
-# print(transactions_dictionary)
+# pprint(transactions_dictionary)
+
+all_customer_data = {}
+for cust_nums in customer_master:
+    str_num = f'{cust_nums}'
+    balance_due = round(float(customer_master[str_num]['balance']), 2)
+    print(f'Customer Number {str_num} has a balance of {balance_due}')
+    print(f"{str_num} list of transactions are:")
+    all_customer_data[str_num] = {
+        "name": cust_nums,
+        "previous balance": balance_due,
+        "discount": customer_master[str_num]['discount'],
+        "customer number": str_num,
+        "transactions": [],
+        "current balance": 0
+    }
+    for transaction in transactions_dictionary[str_num]:
+        cost = 0.0
+        if transaction[0] == "O":
+            try:
+                cost = float(transaction[4]) * float(transaction[5]) * (1 - (float(customer_master[str_num]['discount'])/100))
+            except ValueError:
+                cost = float(transaction[4]) * float(transaction[5]) * 1
+            try:
+                cost = cost * (1 - (float(transaction[6])/100))
+            except ValueError:
+                cost = cost * 1
+            print(f"{str_num} bought ${cost} of {transaction[3]}")
+            balance_due += cost
+            transaction_info = [transaction[2], transaction[3], cost, round(float(transaction[4]) * float(transaction[5]) - cost, 2)]
+        else:
+            print(f"{str_num} made a payment of ${float(transaction[4])}") 
+            try:
+                cost = round(float(transaction[4]) * (1 + (float(transaction[6])/100)), 2)
+                # balance_due -= float(transaction[4])
+            except ValueError:
+                cost = float(transaction[4])
+            balance_due += cost
+            transaction_info = [transaction[2], transaction[3], cost]
+        all_customer_data[str_num]["transactions"].append(transaction_info)
+        all_customer_data[str_num]["current balance"] = balance_due
+        
+
+pprint(all_customer_data)
+    # print(f"{str_num} owes ${balance_due}")
+
+    # print("\n")
+
+
+# transaction number		item ordered	  order amount     discounted amount
+# transaction number		item ordered	  order amount     discounted amount 
+# transaction number 	payment	  payment amt      discounted pmt amt
+# transaction number		item ordered	  order amount      discounted amount
+
 
