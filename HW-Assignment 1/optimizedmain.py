d087f29596004a7acc3746c6588fba474084f9d9
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-09-02 21:03:27 -0400

    Update optimizedmain.py

--- HW-Assignment 1/optimizedmain.py
+++ HW-Assignment 1/optimizedmain.py
@@ -56,7 +56,7 @@ for transaction in transactions_list:
             cost = cost * 1
         # print(f"{str_num} bought ${cost} of {transaction[3]}")
         balance_due += cost
-        customer_master[transaction[1]]["transactions"] += f"TRANSACTION NUMBER:{transaction[2]} | TRANSACTION ITEM:{transaction[3]} | PAYMENT AMOUNT: {cost} | PAYMENT DISCOUNT: {round(float(transaction[4]) * float(transaction[5]) - cost)}\n"
+        customer_master[transaction[1]]["transactions"] += f"TRANSACTION NUMBER:{transaction[2]} | TRANSACTION ITEM:{transaction[3]} | PAYMENT AMOUNT: ${cost} | PAYMENT DISCOUNT: ${round(float(transaction[4]) * float(transaction[5]) - cost)}\n"
     else:
         # print(f"{str_num} made a payment of ${float(transaction[4])}") 
         try:
@@ -65,7 +65,7 @@ for transaction in transactions_list:
         except ValueError:
             cost = float(transaction[4])
         balance_due -= cost
-        customer_master[transaction[1]]["transactions"] += f"TRANSACTION NUMBER:{transaction[2]} | TRANSACTION TYPE:{transaction[3]} | PAYMENT AMOUNT: { round(float(transaction[4]), 2)} | PAYMENT DISCOUNT: {cost}\n"
+        customer_master[transaction[1]]["transactions"] += f"TRANSACTION NUMBER:{transaction[2]} | TRANSACTION TYPE:{transaction[3]} | PAYMENT AMOUNT: ${round(float(transaction[4]), 2)} | PAYMENT DISCOUNT: ${cost}\n"
     customer_master[transaction[1]]['balance'] = balance_due
     # print(f"AFTER BALANCE DUE: {balance_due}")   
 
